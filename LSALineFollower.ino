ffa75587807f1a38e54f2ae829ecb6cd77c71531
Arnav Brahme <arnav@Arnavs-MacBook-Air-2.local>
2026-03-25 09:56:21 +0530

    Added Logger class use in .ino

--- LSALineFollower.ino
+++ LSALineFollower.ino
@@ -2,14 +2,16 @@
 #include "driveControl.h"
 #include "PIDControl.h"
 #include "StateControl.h"
+#include "Logger.h"
 
 const int LSA_ANPin = 34;
 const int LSA_JPin = 35;
 const int endPointLED = 13;
 
-PIDControl PID(1.0, 0.0, 0.5);
+PIDControl PID(1.0, 0.0, 0.0);
 driveControl drive(25, 26, 33, 27, 14, 12);
 LSA08 SensorArray(LSA_ANPin, LSA_JPin);
+Logger debugLogger(50);
 
 StateControl StateMachine(SensorArray, PID, drive, 13);
 
@@ -21,6 +23,7 @@ void setup(){
 
   SensorArray.Initialise();
   drive.initialise();
+  debugLogger.begin(115200);
 
   drive.setDefaultSpeed(150);
 
@@ -31,4 +34,13 @@ void setup(){
 
 void loop(){
   StateMachine.update();
+
+  float currentError = SensorArray.getPositionError();
+  float pidCorrection = PID.compute(currentError);
+
+  int baseSpeed = 150;
+  int leftSpeed = baseSpeed + pidCorrection;
+  int rightSpeed = baseSpeed - pidCorrection;
+
+  debugLogger.logData(currentError, pidCorrection, leftSpeed, rightSpeed);
 }
